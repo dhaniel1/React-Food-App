1d4599a41aa91956f20ce48bc80412e199db20ab
dhaniel1 <obioradaniel60@gmail.com>
2023-03-24 16:17:45 +0100

    Added bumpy button feature

--- src/Component/Layout/HeaderCartButton.js
+++ src/Component/Layout/HeaderCartButton.js
@@ -1,18 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../Store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import style from "./HeaderCartButton.module.css";
 
 export default function HeaderCartButton(props) {
   const ctx = useContext(CartContext);
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 
   const numberOfCartItems = ctx.items.reduce((curNumber, item) => {
     // Items is an array of objects
     return curNumber + item.amount;
   }, 0);
+  const { items } = ctx;
+  const btnClasses = `${style.button} ${btnIsHighlighted ? style.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
 
   return (
-    <button className={style.button} onClick={props.showCart}>
+    <button className={btnClasses} onClick={props.showCart}>
       <span className={style.icon}>
         <CartIcon />
       </span>
